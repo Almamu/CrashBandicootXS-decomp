62a3c9c4583e5585e9a3ea2d3148403130e1f1a1
Alexis Maiquez Murcia <decomp-crash-xs@alma.mu>
2025-11-24 03:56:30 +0100

    reverse some irq structs/typedefs

--- src/irq.c
+++ src/irq.c
@@ -1,20 +1,21 @@
 #include "core.h"
+#include "irq.h"
 
-extern void* gUnknown_030009E8[5];
-extern void* gUnknown_03000A20[5];
+extern irq_handler_t* gUnknown_030009E8[5];
+extern irq_handler_t* gUnknown_03000A20[5];
 void nullsub_10();
 extern u32 IntrMain_Buffer;
 
 // TODO: PROPERLY UNDERSTAND WHAT THIS FUNCTION IS USED FOR, MIGHT BE A CALLBACK INIT FUNCTION FOR SOME TYPE OF MESSAGE
 // irq_handler_clear_index maybe?
-void sub_8000544(s32 arg0) {
-    gUnknown_030009E8[arg0] = &nullsub_10;
+void sub_8000544(s32 interruptIndex) {
+    gUnknown_030009E8[interruptIndex] = &nullsub_10;
 }
 
 // TODO: PROPERLY NAME THIS FUNCTION, SEEMS TO BE DISABLING SPECIFIC INTERRUPT HANDLING
 // irq_disable_index maybe?
 void sub_8000558(s32 interruptIndex) {
-    u32* tmp = gUnknown_030009E8[interruptIndex] = gUnknown_03000A20[interruptIndex];
+    irq_handler_t* tmp = gUnknown_030009E8[interruptIndex] = gUnknown_03000A20[interruptIndex];
     
     if (tmp == NULL) {
         u16 previousIMEvalue = REG_IME;
@@ -28,9 +29,9 @@ void sub_8000558(s32 interruptIndex) {
 
 // TODO: PROPERLY NAME THIS FUNCTION
 // irq_enable_index or maybe irq_set_index
-void sub_80005A0(s32 interruptIndex, s32 arg1) {
+void sub_80005A0(s32 interruptIndex, irq_handler_t* fn) {
     gUnknown_03000A20[interruptIndex] = gUnknown_030009E8[interruptIndex];
-    gUnknown_030009E8[interruptIndex] = arg1;
+    gUnknown_030009E8[interruptIndex] = fn;
     REG_IE |= 1 << interruptIndex;
 }
 
@@ -41,15 +42,14 @@ void irq_disable(void) {
 
 u32 irq_setup() {
     u32* intrbuffer = &IntrMain_Buffer;
-    u32 data = &nullsub_10;
-    u32** dst1 = (u32** )0x03000A20;
-    u32** dst2 = (u32** )0x030009E8;
-    s32 count = 0xD;
+    irq_handler_t* fn = &nullsub_10;
+    irq_handler_t** dst1 = &gUnknown_03000A20;
+    irq_handler_t** dst2 = &gUnknown_030009E8;
+    s32 count;
 
-    for (;count >= 0;) {
-        *dst1++ = data;
-        *dst2++ = data;
-        count --;
+    for (count = 0xD; count >= 0; count --) {
+        *dst1++ = fn;
+        *dst2++ = fn;
     }
     
     INTR_VECTOR = intrbuffer;
